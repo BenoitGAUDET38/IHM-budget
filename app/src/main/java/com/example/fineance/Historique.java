06b162b777796883b7381a2c77d8ebf308bcd052
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-03-30 16:48:44 +0200

    LE COMMIT ORIGINEL

--- app/src/main/java/com/example/fineance/Historique.java
+++ app/src/main/java/com/example/fineance/Historique.java
@@ -11,4 +11,5 @@ public class Historique extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
 }
--- app/src/main/java/com/example/fineance/MainActivity.java
+++ app/src/main/java/com/example/fineance/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.fineance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView moneyCircleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moneyCircleButton=findViewById(R.id.moneyCircleButton);
+
+        moneyCircleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newActivity=new Intent(MainActivity.this, Historique.class);
+                startActivity(newActivity);
+            }
+        });
     }
 }
