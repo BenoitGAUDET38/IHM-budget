d551e7df95444835b4a1b0aee9512e038673e754
Benoit Gaudet <bgaudet38@gmail.com>
2022-03-30 17:23:27 +0200

    Super bottom bar made home (india home)

--- app/src/main/java/com/example/fineance/MainActivity.java
+++ app/src/main/java/com/example/fineance/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.fineance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bot_nav_bar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new HomeFragment()).commit();
+
+        bottomNav.setSelectedItemId(R.id.nav_home);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_categorie:
+                        fragment = new CategorieFragment();
+                        break;
+
+                    case R.id.nav_prevision:
+                        fragment = new PrevisionFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
+
+                return true;
+            }
+        });
     }
 }
