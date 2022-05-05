46e991250b2c8c65174131c15d60a7d2d792f316
JeannesTheo <theo.jeannes@etu.unice.fr>
2022-05-05 12:44:15 +0200

    Fragment Dynamique :Add & Cancel

--- app/src/main/java/com/example/fineance/controller/inputFragment/DepenseFragment.java
+++ app/src/main/java/com/example/fineance/controller/inputFragment/DepenseFragment.java
@@ -1,57 +1,107 @@
 package com.example.fineance.controller.inputFragment;
 
+import static android.app.Activity.RESULT_OK;
+import static com.example.fineance.controller.spendingActivity.ScanningActivity.CAMERA_REQUEST_CODE;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.fineance.R;
+import com.example.fineance.controller.spendingActivity.ScanningActivity;
 import com.example.fineance.model.Depense;
 
-public class DepenseFragment extends Fragment{
+public class DepenseFragment extends Fragment {
+
+    private final ActivityResultLauncher<Intent> activityResultLaunch = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null && result.getData().getExtras() != null) {
+                    Bundle extras = result.getData().getExtras();
+                    if (extras.containsKey("montant") && extras.get("montant") != null) {
+                        this.montantEditText.setText(extras.get("montant").toString());
+                    }
+                }
+            });
+
+    /**
+     * TODO Date ?
+     **/
+
     private EditText nomEditText;
     private EditText categorieEditText;
     private EditText provenanceEditText;
     private EditText commentaireEditText;
     private Button validerButton;
     private Button annulerButton;
+    private EditText montantEditText;
 
-     public DepenseFragment() {
+    public DepenseFragment() {
         // Required empty public constructor
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            if (getArguments() != null) {
-                validerButton.setText(getArguments().getString("ADD"));
-                annulerButton.setText(getArguments().getString("REMOVE"));
-            }
+        if (getArguments() != null) {
+            validerButton.setText(getArguments().getString("ADD"));
+            annulerButton.setText(getArguments().getString("REMOVE"));
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_depense, container, false);
         validerButton = view.findViewById(R.id.ajout_depense_valide_button);
         annulerButton = view.findViewById(R.id.ajout_depense_annuler_button);
+        montantEditText = view.findViewById(R.id.ajout_depense_montant_editText);
         nomEditText = view.findViewById(R.id.ajout_depense_nom_edit_text);
         categorieEditText = view.findViewById(R.id.ajout_depense_catégorie_edit_text);
         provenanceEditText = view.findViewById(R.id.ajout_depense_provenance_edit_text);
         commentaireEditText = view.findViewById(R.id.ajout_depense_commentaire_edit_text);
+
+        ImageView ticketScanner = view.findViewById(R.id.ticketScannerImageView);
+        ticketScanner.setOnClickListener(viewScanner -> {
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+            }
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                this.activityResultLaunch.launch(new Intent(getActivity().getApplicationContext(), ScanningActivity.class));
+            }
+        });
+
         view.findViewById(R.id.ajout_depense_annuler_button).setOnClickListener(viewRemove -> {
             Bundle result = new Bundle();
-            result.putParcelable("delete",null);
-            getParentFragmentManager().setFragmentResult("delete",result);
+            result.putParcelable("delete", null);
+            getParentFragmentManager().setFragmentResult("delete", result);
         });
         view.findViewById(R.id.ajout_depense_valide_button).setOnClickListener(viewClicked -> {
-            Depense d = new Depense("Fragment",0,"To",20,"USD","Ajout des fragments dynamiques :p");
             Bundle result = new Bundle();
-            result.putParcelable("transaction",d);
-            getParentFragmentManager().setFragmentResult("transaction",result);
+            result.putParcelable("transaction", retrieveDepenseFromForm());
+            getParentFragmentManager().setFragmentResult("transaction", result);
         });
         return view;
+    }
+
+    private Depense retrieveDepenseFromForm() {
+        String name = String.valueOf(nomEditText.getText());
+        int categorie = Integer.parseInt(String.valueOf(categorieEditText.getText()));
+        String provenance = String.valueOf(provenanceEditText.getText());
+        double montant = Double.parseDouble(String.valueOf(montantEditText.getText()));
+        String devise = "USD";
+        String commentaire = String.valueOf(commentaireEditText.getText());
+        return new Depense(name, categorie, provenance, montant, devise, commentaire);
     }
 }
--- app/src/main/java/com/example/fineance/controller/spendingActivity/AddExpenseActivity.java
+++ app/src/main/java/com/example/fineance/controller/spendingActivity/AddExpenseActivity.java
@@ -1,77 +1,34 @@
 package com.example.fineance.controller.spendingActivity;
 
-import static com.example.fineance.controller.spendingActivity.ScanningActivity.CAMERA_REQUEST_CODE;
 import static com.example.fineance.model.PerformNetworkRequest.createTransaction;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.ImageView;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.example.fineance.R;
 import com.example.fineance.controller.inputFragment.DepenseFragment;
 import com.example.fineance.model.Depense;
 
-public class AddExpenseActivity extends AppCompatActivity{
-
-
-    private EditText montantEditText;
-    private final ActivityResultLauncher<Intent> activityResultLaunch = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == RESULT_OK && result.getData() != null && result.getData().getExtras() != null) {
-                    Bundle extras = result.getData().getExtras();
-                    if (extras.containsKey("montant") && extras.get("montant") != null) {
-                        this.montantEditText.setText(extras.get("montant").toString());
-                    }
-//                    if (extras.containsKey("commentaire") && extras.get("commentaire") != null) {
-//                        this.commentaireEditText.setText(extras.get("commentaire").toString());
-//                    }
-                }
-            });
+public class AddExpenseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajout_depense);
-        montantEditText = findViewById(R.id.ajout_depense_montant_editText);
-        /*validerButton.setOnClickListener(view -> {
-            Intent ajouteDepenseIntent = new Intent(this, MainActivity.class);
-            Depense depense = new Depense(*//*String.valueOf(nomEditText.getText())*//*"", *//*Integer.parseInt(String.valueOf(categorieEditText.getText()))*//*0, *//*String.valueOf(provenanceEditText.getText())*//*"", Double.parseDouble(String.valueOf(montantEditText.getText())), "EUR", ""*//*String.valueOf(commentaireEditText.getText())*//*);
-            ajouteDepenseIntent.putExtra("depense", (Parcelable) depense);
-            startActivity(ajouteDepenseIntent);
-            finish();
-        });*/
-
-        ImageView ticketScanner = this.findViewById(R.id.ticketScannerImageView);
-        ticketScanner.setOnClickListener(view -> {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
-            }
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                this.activityResultLaunch.launch(new Intent(this.getApplicationContext(), ScanningActivity.class));
-            }
-        });
 
         getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new DepenseFragment()).commit();
+        setListeners();
+    }
 
+    private void setListeners() {
         getSupportFragmentManager().setFragmentResultListener("transaction", this, (requestKey, result) -> {
             createTransaction((Depense) result.get(requestKey));
             finish();
         });
-
         getSupportFragmentManager().setFragmentResultListener("delete", this, (requestKey, result) -> {
-            //createTransaction((Depense) result.get(requestKey));
-            Log.d("DEBUG","Travail Terminé, on plie les gaules");
+            Log.d("DEBUG", "Travail Terminé, on plie les gaules");
             finish();
         });
     }
