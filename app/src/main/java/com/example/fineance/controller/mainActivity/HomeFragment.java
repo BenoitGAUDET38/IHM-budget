f9767227d02da6428eaed311b5a472a002a19927
MatisPrograms <matis.herrmann@etu.unice.fr>
2022-05-09 09:49:54 +0200

    Changed the layout of the Home_fragment

--- app/src/main/java/com/example/fineance/controller/mainActivity/HomeFragment.java
+++ app/src/main/java/com/example/fineance/controller/mainActivity/HomeFragment.java
@@ -1,7 +1,6 @@
 package com.example.fineance.controller.mainActivity;
 
 import static com.example.fineance.model.PerformNetworkRequest.getDepenses;
-import static java.util.Objects.isNull;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -29,9 +28,8 @@ import com.example.fineance.controller.spendingActivity.AddExpenseActivity;
 public class HomeFragment extends Fragment {
     public static String TOTAL_KEY = "total";
     public static String DEVISE_KEY = "devise";
-
-    private double totalDepenses=0;
-    private String devise = "€";
+    private final String devise = "€";
+    private double totalDepenses = 0;
 
     public HomeFragment() {
     }
@@ -44,7 +42,7 @@ public class HomeFragment extends Fragment {
 
         TextView montant = view.findViewById(R.id.info_total);
         montant.setText(totalDepenses + this.devise);
-        Log.d("DEBUG","Create");
+        Log.d("DEBUG", "Create");
         return view;
     }
 
@@ -60,20 +58,14 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ImageView moneyCircle = this.requireView().findViewById(R.id.moneyCircleButton);
-        moneyCircle.setOnClickListener(v -> {
-            this.startActivity(new Intent(this.getActivity(), AddExpenseActivity.class));
-        });
+        moneyCircle.setOnClickListener(v -> this.startActivity(new Intent(this.getActivity(), AddExpenseActivity.class)));
         ImageView account = this.requireView().findViewById(R.id.accountOptionImageView);
-        account.setOnClickListener(v -> {
-            this.startActivity(new Intent(getActivity(), SettingsActivity.class));
-        });
+        account.setOnClickListener(v -> this.startActivity(new Intent(getActivity(), SettingsActivity.class)));
     }
 
-    public void updateTotal(double total){
-        if(!isNull(getActivity())){
-            TextView montant = getActivity().findViewById(R.id.info_total);
-            totalDepenses = total;
-            montant.setText(totalDepenses+devise);
-        }
+    public void updateTotal(double total) {
+        TextView montant = this.requireActivity().findViewById(R.id.info_total);
+        totalDepenses = total;
+        montant.setText(totalDepenses + devise);
     }
 }
