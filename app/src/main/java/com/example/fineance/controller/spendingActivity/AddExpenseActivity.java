99493ef910e702d46cdfbae78d440ecf494435e1
Benoit Gaudet <bgaudet38@gmail.com>
2022-05-11 13:53:00 +0200

    ajoute notif depassement de seuil

--- app/src/main/java/com/example/fineance/controller/spendingActivity/AddExpenseActivity.java
+++ app/src/main/java/com/example/fineance/controller/spendingActivity/AddExpenseActivity.java
@@ -3,6 +3,8 @@ package com.example.fineance.controller.spendingActivity;
 import static com.example.fineance.controller.inputFragment.DepenseFragment.newDepenseFragment;
 import static com.example.fineance.model.PerformNetworkRequest.createTransaction;
 import static com.example.fineance.model.PerformNetworkRequest.deleteTransaction;
+import static com.example.fineance.model.PerformNetworkRequest.depenseList;
+import static com.example.fineance.model.PerformNetworkRequest.getCategories;
 import static com.example.fineance.model.PerformNetworkRequest.updateTransaction;
 import static java.util.Objects.isNull;
 
@@ -15,13 +17,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.fineance.R;
 import com.example.fineance.controller.inputFragment.DepenseFragment;
+import com.example.fineance.model.Categorie;
 import com.example.fineance.model.Depense;
+import com.example.fineance.model.DepenseUtilities;
 import com.example.fineance.model.notifications.Notification;
 import com.example.fineance.model.notifications.notificationsFactories.AbstractNotificationFactory;
 import com.example.fineance.model.notifications.notificationsFactories.HighPriorityNotificationFactory;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class AddExpenseActivity extends AppCompatActivity {
     private Depense depense = null;
@@ -78,6 +83,20 @@ public class AddExpenseActivity extends AppCompatActivity {
                         depense.getMontant() + depense.getDevise() + " à " +
                         depense.getProvenance() + " a été ajouté !");
         notif.sendNotificationOnChannel();
+        List<Categorie> categories = getCategories();
+        double currentSeuil = 100;
+        for (Categorie c : categories) {
+            if (c.getId() == depense.getCategorie()) {
+                currentSeuil = c.getSeuil();
+            }
+        }
+        if (currentSeuil <= DepenseUtilities.getMontantTotalParCategorie(depenseList, depense.getCategorie()) + DepenseUtilities.getDepenseConvertion(depense)) {
+            Notification notifPrevention = factory.buildBasicNotification(getApplicationContext(),
+                    "ATTENTION SEUIL",
+                    "En ajoutant " + depense.getNom() + " d'une valeur de " +
+                            depense.getMontant() + depense.getDevise() + ", vous avez dépassé votre seuil fixé à " + currentSeuil + "EUR.");
+            notifPrevention.sendNotificationOnChannel();
+        }
     }
 
     public void saveData(String s) {
